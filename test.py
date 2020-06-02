4b57b1762d1b79cc322538336d152b78ca23f216
superuserx <simpson3@gmx.de>
2020-06-02 13:24:51 +0200

    funzt

--- test.py
+++ test.py
@@ -12,8 +12,7 @@ load_contrib('automotive.ecu')
 load_contrib('cansocket')
 
 sock1 = ISOTPSocket('vcan0', sid=0x701, did=0x601, basecls=UDS)
-transfer_ready = False
-transfer_done = False
+
 
 def securityAccess(resp, req):
     if req.service + 0x40 != resp.service or len(req) < 2:
@@ -34,60 +33,21 @@ def securityAccess(resp, req):
     return False
 
 
-def transferData(resp, req):
-    global transfer_ready, transfer_done
-
-    if (req.service + 0x40) == resp.service and len(req) > 1:
-        if req.blockSequenceCounter == 0 and transfer_ready:
-            transfer_ready = False
-            transfer_done = True
-            #new_fw = open("firmware/rec_random.txt", "wb")
-            data = req.transferRequestParameterRecord.decode("utf-8")
-            print(data)
-            #new_fw.write(data)
-            #new_fw.close()
-            return True
-    return False
-
-
-def uploadRequest(resp, req):
-    global transfer_ready, transfer_done
-
-    if (req.service + 0x40) == resp.service and len(req) > 2:
-        if req.memorySizeLen == 0x1 and req.memoryAddressLen == 0x4 and not transfer_done:
-            transfer_ready = True
-            print("Ready for Transfer")
-            return True
-    return False
-
-
-def transferExit(resp, req):
-    global transfer_done
-
-    if (req.service + 0x40) == resp.service and len(req) > 1:
-        if req.transferRequestParameterRecord == b'done' and transfer_done:
-            transfer_done = False
-            return True
-    elif hasattr(resp, 'negativeResponseCode') and req.service == 0x37:
-        if resp.negativeResponseCode == 0x22:
-            return True
-    return False
-
-
 responseList = [ECUResponse(session=2, responses=UDS() / UDS_SAPR(), answers=securityAccess),
                 ECUResponse(session=2, responses=UDS() / UDS_NR(requestServiceId=0x27, negativeResponseCode=0x33), answers=securityAccess),
 
                 ECUResponse(session=range(255), security_level=range(255), responses=UDS() / UDS_DSCPR(diagnosticSessionType=0x01)),
                 ECUResponse(session=1, security_level=range(255), responses=UDS() / UDS_DSCPR(diagnosticSessionType=0x02)),
 
-                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_TDPR(), answers=transferData),
+                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_TDPR()),
                 ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_NR(requestServiceId=0x36, negativeResponseCode=0x22)),
 
-                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_RUPR(), answers=uploadRequest),
+                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_RUPR()),
                 ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_NR(requestServiceId=0x35, negativeResponseCode=0x13)),
 
-                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_RTEPR(transferResponseParameterRecord=b'ok'), answers=transferExit),
-                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_NR(requestServiceId=0x37, negativeResponseCode=0x22), answers=transferExit),
+                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_RTEPR(transferResponseParameterRecord=b'ok')),
+                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_NR(requestServiceId=0x37, negativeResponseCode=0x22)),
+                ECUResponse(session=2, security_level=range(1, 255), responses=UDS() / UDS_NR(requestServiceId=0x38, negativeResponseCode=0x22)),
                 ]
 
 answering_machine1 = ECU_am(supported_responses=responseList, main_socket=sock1, basecls=UDS, timeout=None)
